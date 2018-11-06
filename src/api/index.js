7d6d52d05979324a4c4d9d915a963b6ffd2f59ee
maopixin <18364076997@163.com>
2018-11-06 15:18:23 +0800

    6

--- src/api/index.js
+++ src/api/index.js
@@ -12,7 +12,6 @@ axios.interceptors.request.use(
     });
 
 // // http response 服务器响应拦截器，这里拦截401错误，并重新跳入登页重新获取token
-//
 // axios.interceptors.response.use(
 //     response => {
 //         return response;
